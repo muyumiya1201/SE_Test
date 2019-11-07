da715f9110764025d998ac17eb2c33f0b061566b
PeggyLi <suara1201fxt@gmail.com>
2019-11-07 18:19:35 +0800

    fix conflict

---
+++ app_run.py
@@ -0,0 +1,14 @@
+from flask import *
+from datetime import datetime
+import Calculator as calculator
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    data = "Deploying a Flask App To Heroku"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
